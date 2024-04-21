dbd2511049032e2ae327aaab9000be849c92c376
Kristof Schroeder <kristof_schroeder@web.de>
2024-04-21 20:36:00 +0200

    Add docstrings for metrics module

--- malaria/eval/metrics.py
+++ malaria/eval/metrics.py
@@ -9,22 +9,36 @@ if TYPE_CHECKING:
 
 T = TypeVar("T")
 
-UNINFECTED_VAL = 1
-PARASITIZED_VAL = 0
-
 
 class EvalMetric(Generic[T], ABC):
+    """
+    Abstract base class for evaluation metrics.
+    """
     @property
     @abstractmethod
     def name(self) -> str:
-        pass
+        """
+       Should return the name of the metric.
+       """
 
     @abstractmethod
     def compute(self, eval_input: "InfluenceEvaluationInput") -> List[T]:
-        pass
+        """
+        Computes the metric based on the provided InfluenceEvaluationInput.
+
+        Args:
+            eval_input: The evaluation input data.
+
+        Returns:
+            A list containing the computed metric results.
+        """
 
 
 class MeanInfluence(EvalMetric[float]):
+    """
+    Metric that calculates the mean influence value across all test data points.
+    """
+
     MEAN_INFLUENCE_METRIC_NAME = "mean_influence"
 
     @property
@@ -36,6 +50,11 @@ class MeanInfluence(EvalMetric[float]):
 
 
 class MeanInfluenceUnInfected(EvalMetric[float]):
+    """
+    Metric that calculates the mean influence value across all uninfected test
+    data points.
+    """
+
     MEAN_INFLUENCE_UNINFECTED_METRIC_NAME = "mean_influence_uninfected"
 
     @property
@@ -43,15 +62,16 @@ class MeanInfluenceUnInfected(EvalMetric[float]):
         return self.MEAN_INFLUENCE_UNINFECTED_METRIC_NAME
 
     def compute(self, eval_input: InfluenceEvaluationInput) -> List[float]:
-        uninfected_test_indices = [
-            idx
-            for idx, value in enumerate(eval_input.test_labels)
-            if value == UNINFECTED_VAL
-        ]
+        uninfected_test_indices = eval_input.uninfected_test_indices
         return eval_input.influence_value_array[uninfected_test_indices, :].mean(axis=0)
 
 
 class MeanInfluenceParasitized(EvalMetric[float]):
+    """
+    Metric that calculates the mean influence value across all parasitized test
+    data points.
+    """
+
     MEAN_INFLUENCE_UNINFECTED_METRIC_NAME = "mean_influence_parasitized"
 
     @property
@@ -59,17 +79,18 @@ class MeanInfluenceParasitized(EvalMetric[float]):
         return self.MEAN_INFLUENCE_UNINFECTED_METRIC_NAME
 
     def compute(self, eval_input: InfluenceEvaluationInput) -> List[float]:
-        parasitized_test_indices = [
-            idx
-            for idx, value in enumerate(eval_input.test_labels)
-            if value == PARASITIZED_VAL
-        ]
+        parasitized_test_indices = eval_input.parasitized_test_indices
         return eval_input.influence_value_array[parasitized_test_indices, :].mean(
             axis=0
         )
 
 
 class QuantileInfluence(EvalMetric):
+    """
+    Metric that calculates a specific quantile of the influence values across all test
+    data points.
+    """
+
     QUANTILE_INFLUENCE_SUFFIX = "_quantile_influence"
 
     def __init__(self, quantile: float):
@@ -84,6 +105,11 @@ class QuantileInfluence(EvalMetric):
 
 
 class QuantileInfluenceUninfected(EvalMetric):
+    """
+    Metric that calculates a specific quantile of the influence values across all
+    uninfected test data points.
+    """
+
     QUANTILE_INFLUENCE_SUFFIX = "_quantile_influence_uninfected"
 
     def __init__(self, quantile: float):
@@ -94,16 +120,17 @@ class QuantileInfluenceUninfected(EvalMetric):
         return f"{self.quantile}{self.QUANTILE_INFLUENCE_SUFFIX}"
 
     def compute(self, eval_input: InfluenceEvaluationInput) -> List[float]:
-        uninfected_test_indices = [
-            idx
-            for idx, value in enumerate(eval_input.test_labels)
-            if value == UNINFECTED_VAL
-        ]
+        uninfected_test_indices = eval_input.uninfected_test_indices
         uninfected_array = eval_input.influence_value_array[uninfected_test_indices, :]
         return np.quantile(uninfected_array, self.quantile, axis=0)
 
 
 class QuantileInfluenceParasitized(EvalMetric):
+    """
+    Metric that calculates a specific quantile of the influence values across all
+    parasitized test data points.
+    """
+
     QUANTILE_INFLUENCE_SUFFIX = "_quantile_influence_parasitized"
 
     def __init__(self, quantile: float):
@@ -114,31 +141,11 @@ class QuantileInfluenceParasitized(EvalMetric):
         return f"{self.quantile}{self.QUANTILE_INFLUENCE_SUFFIX}"
 
     def compute(self, eval_input: InfluenceEvaluationInput) -> List[float]:
-        parasitized_test_indices = [
-            idx
-            for idx, value in enumerate(eval_input.test_labels)
-            if value == UNINFECTED_VAL
-        ]
+        parasitized_test_indices = eval_input.parasitized_test_indices
         parasitized_array = eval_input.influence_value_array[
             parasitized_test_indices, :
         ]
         return np.quantile(parasitized_array, self.quantile, axis=0)
-
-
-DEFAULT_METRICS = [
-    MeanInfluence(),
-    MeanInfluenceUnInfected(),
-    MeanInfluenceParasitized(),
-    QuantileInfluence(0.05),
-    QuantileInfluenceUninfected(0.05),
-    QuantileInfluenceParasitized(0.05),
-    QuantileInfluence(0.10),
-    QuantileInfluenceUninfected(0.10),
-    QuantileInfluenceParasitized(0.10),
-    QuantileInfluence(0.25),
-    QuantileInfluenceUninfected(0.25),
-    QuantileInfluenceParasitized(0.25),
-]
 
 
 class DefaultMetrics(Enum):
