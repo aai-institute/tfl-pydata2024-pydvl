20b36eed3b3a4082c1c7768413e9a8e73f13f1c3
Miguel de Benito Delgado <m.debenito.d@gmail.com>
2024-04-22 11:09:36 +0200

    Add analytics

--- slides/global-bottom.vue
+++ slides/global-bottom.vue
@@ -1,10 +1,26 @@
+<script setup>
+import { onMounted } from "vue";
+
+onMounted(() => {
+  const script = document.createElement("script");
+  script.async = true;
+  script.defer = true;
+  script.src = "https://scripts.simpleanalyticscdn.com/latest.js";
+  document.head.appendChild(script);
+});
+</script>
+
 <template>
   <div v-if="$slidev.nav.currentLayout !== 'cover'">
-    <div class="absolute text-xs bottom-0 right-0 w-full flex justify-center items-center">
-        <span><SlideCurrentNo /> / <SlidesTotal /></span>
+    <div
+      class="absolute text-xs bottom-0 right-0 w-full flex justify-center items-center"
+    >
+      <span><SlideCurrentNo /> / <SlidesTotal /></span>
     </div>
-    <img class="absolute bottom-1 right-1 w-40 z-9999"
-        src="/transferlab-logo.svg"
-        alt="TransferLab logo" />
+    <img
+      class="absolute bottom-1 right-1 w-40 z-9999"
+      src="/transferlab-logo.svg"
+      alt="TransferLab logo"
+    />
   </div>
 </template>
